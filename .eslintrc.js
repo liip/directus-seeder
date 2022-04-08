fb7f59f1c7293313737d880c0c6a0a70d6ac041d
Juerg Hunziker <juerg.hunziker@liip.ch>
2022-04-08 14:46:03 +0200

    fix prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,12 @@ module.exports = {
   env: {
     node: true,
   },
-  "root": true,
-  "parser": "@typescript-eslint/parser",
-  "plugins": ["@typescript-eslint", "prettier"],
-  "extends": ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
-  rules: {
-    // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
-  },
-
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
 };
